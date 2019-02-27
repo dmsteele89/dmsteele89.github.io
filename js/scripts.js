94a669f8237ba874dd8343344894ce2187301940
Steele <dsteele5@lab-che204-m16.eastern.ewu.edu>
2019-02-27 11:09:42 -0800

    Start of Portfolio

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Everything OK?
+console.log('OK');
+
+// Your Awesome Scripts
+$(document).ready(function(){
+    // Target your .container, .wrapper, .post, etc.
+    $("main").fitVids();
+  });
